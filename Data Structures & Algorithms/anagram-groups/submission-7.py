cd57ec62e6d6ac3f149fcd4994d23379d8bdd673
Sebrey-young <sebasreybrtltt@gmail.com>
2026-08-29 15:11:38 -0400

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+
+        for words in strs:
+            count = [0] * 26
+
+            for chars in words:
+                count [ord(chars) - ord("a")] += 1
+
+            res[tuple(count)].append(words)
+
+        return list(res.values())
